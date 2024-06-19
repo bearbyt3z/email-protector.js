9cd3158c9d862f34113895194e3d19fb6580f603
bearbyt3z <bearbyt3z@gmail.com>
2024-06-19 03:09:19 +0200

    Replaced deprecated substr() with substring() function

--- src/tools.js
+++ src/tools.js
@@ -7,7 +7,7 @@ const Tools = {
   reverseString: (str) => String(str).split('').reverse().join(''),
   getRandomString: (length = 8, charset = 'abcdefghijklmnopqrstuvwxyz') => Array.from({ length }, () => charset[Math.floor(Math.random() * charset.length)]).join(''),
   getRandomEmail: () => `${Tools.getRandomString(6)}@${Tools.getRandomString(10)}.${Tools.getRandomString(3)}`,
-  getUniqID: () => `_${Date.now().toString(36) + Math.random().toString(36).substr(2)}`,
+  getUniqID: () => `_${Date.now().toString(36) + Math.random().toString(36).substring(2)}`,
 };
 
 export default Tools;
